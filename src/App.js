912f4b9c014b5be1e5ed71d1da1a238530e0acdd
rbevilaqua12 <ryan.bevilaqua05@gmail.com>
2024-02-28 13:09:15 -0300

    Project-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from './styles';
+import People from './assets/people.svg'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Image alt="logo-img" src="{People}"/>
+      <ContainerItens>
+
+        <H1>Olá</H1>
+
+        <InputLabel>Nome</InputLabel>
+
+        <Input placeholder='Nome'></Input>
+        
+        <Input placeholder='Idade'></Input>
+
+        <Button>Cadastrar</Button>
+      
+      </ContainerItens>
+    </Container>
   );
 }
 
-export default App;
+export default App  
